ce50b95f5dc9bb0259bbbdb967030cfb955bfb56
maxgunter99 <maxgunter99@gmail.com>
2019-06-12 17:37:24 -0500

    Added Best Practives page and navigation foundations

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import BestPracticesScreen from './src/screens/BestPracticesScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default function App(){
+  return <BestPracticesScreen/>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
