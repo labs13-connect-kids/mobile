629497cb80cf7dcdaa84358f941cd7562341fc31
wcolts2000 <wcolts2000@gmail.com>
2019-06-12 10:47:14 -0700

    added packages and dev branch

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -12,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
