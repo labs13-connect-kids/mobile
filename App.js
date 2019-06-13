f19a23afac86f0d905f9c5d2c58bbee271c68df0
wcolts2000 <wcolts2000@gmail.com>
2019-06-13 14:50:02 -0700

    added navigation between views and side drawer functionality

--- App.js
+++ App.js
@@ -1,7 +1,69 @@
-import React from 'react';
-import BestPracticesScreen from './src/screens/BestPracticesScreen';
-// import FamilyConnectionsScreen from './src/screens/FamilyConnectionsScreen';
+import React from "react";
+import BestPracticesScreen from "./src/screens/BestPracticesScreen";
+import FamilyConnectionsScreen from "./src/screens/FamilyConnectionsScreen";
+import {
+  createAppContainer,
+  createDrawerNavigator,
+  createStackNavigator,
+  createSwitchNavigator
+} from "react-navigation";
 
 export default function App() {
-  return <BestPracticesScreen />;
+  return <AppContainer />;
 }
+
+// const AppNavigator = createStackNavigator(
+//   {
+//     BestPractices: {
+//       screen: BestPracticesScreen
+//     },
+//     FamilyConnections: {
+//       screen: FamilyConnectionsScreen
+//     }
+//   },
+//   {
+//     initialRouteName: "BestPractices",
+//   }
+// );
+const BestPracticeNavigator = createStackNavigator(
+  {
+    BestPractices: {
+      screen: BestPracticesScreen
+    }
+  },
+  {
+    initialRouteName: "BestPractices"
+  }
+);
+const FamilyConnectionsNavigator = createStackNavigator(
+  {
+    FamilyConnections: {
+      screen: FamilyConnectionsScreen
+    }
+  },
+  {
+    initialRouteName: "FamilyConnections"
+  }
+);
+
+// const AppContainer = createAppContainer(AppNavigator);
+
+const AppDrawerNavigator = createDrawerNavigator(
+  {
+    BestPractices: {
+      screen: BestPracticeNavigator
+    },
+    FamilyConnections: {
+      screen: FamilyConnectionsNavigator
+    }
+  }
+  // To make drawer open on right side uncomment line below
+  // { drawerPosition: "right" }
+);
+
+const AppSwitchNavigator = createSwitchNavigator({
+  BestPractices: { screen: AppDrawerNavigator },
+  FamilyConnections: { screen: AppDrawerNavigator }
+});
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
