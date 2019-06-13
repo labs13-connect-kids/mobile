afc3109f0879f74caa88498ca8cd3b4acc827df4
Ryan Walker <rytwalker@gmail.com>
2019-06-13 17:35:38 -0400

    people search screen up and state input set up

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import BestPracticesScreen from './src/screens/BestPracticesScreen';
+import PeopleSearchScreen from './src/screens/PeopleSearchScreen';
 // import FamilyConnectionsScreen from './src/screens/FamilyConnectionsScreen';
 
 export default function App() {
-  return <BestPracticesScreen />;
+  return <PeopleSearchScreen />;
 }
