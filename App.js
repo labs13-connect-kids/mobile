cde2cc554a98981a3bd236f3e5790608c2a38b53
maxgunter99 <maxgunter99@gmail.com>
2019-06-13 17:03:33 -0500

    structure added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BestPracticesScreen from './src/screens/BestPracticesScreen';
+import PeopleSearchScreen from './src/screens/PeopleSearchScreen'
 
 export default function App(){
-  return <BestPracticesScreen/>
+  return <PeopleSearchScreen/>
 }
