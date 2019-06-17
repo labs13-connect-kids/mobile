f9617568df239fe900248c9d0b84b8648fb48c96
Ryan Walker <rytwalker@gmail.com>
2019-06-17 13:58:54 -0400

    rendering addresses

--- src/components/PersonInfo.js
+++ src/components/PersonInfo.js
@@ -1,10 +1,60 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+const renderMaskedOrResult = (field, type) => {
+  if (field !== 'value available in full api response') {
+    return field;
+  }
+
+  switch (type) {
+    case 'house':
+      return '****';
+      break;
+    case 'street':
+      return '********* **';
+    case 'zip_code':
+      return '*****';
+    default:
+      break;
+  }
+};
+
 const PersonInfo = ({ item }) => {
   return (
     <View>
       <Text>{item.names[0].display}</Text>
+      <View>
+        <Text>Emails</Text>
+        <View>
+          <Text>Shows emails</Text>
+        </View>
+      </View>
+      <View>
+        <Text>Phone Numbers</Text>
+        <View>
+          <Text>Shows emails</Text>
+        </View>
+      </View>
+      <View>
+        <Text>Addresses</Text>
+        <View>
+          {item.addresses &&
+            item.addresses.map(address => {
+              return (
+                <View>
+                  <Text>
+                    {renderMaskedOrResult(address.house, 'house')}{' '}
+                    {renderMaskedOrResult(address.street, 'street')}
+                  </Text>
+                  <Text>
+                    {address.display}{' '}
+                    {renderMaskedOrResult(address.zip_code, 'zip_code')}
+                  </Text>
+                </View>
+              );
+            })}
+        </View>
+      </View>
     </View>
   );
 };
