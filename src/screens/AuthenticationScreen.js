caa57797db600efb81806f29368cd13124b11929
wcolts2000 <wcolts2000@gmail.com>
2019-07-03 10:52:29 -0700

    changed inport for file reanem

--- src/screens/AuthenticationScreen.js
+++ src/screens/AuthenticationScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { View, StyleSheet } from "react-native";
-import LoginWithAuth0 from "../components/Authentication/LoginWithAuth0";
 import { connect } from "react-redux";
+import LoginWithAuth0 from "./../components/Authentication/LoginWithAuth0";
 import RegisterModalsContainer from "../components/AuthModals/RegisterModalsContainer";
 import {
   setModalVisible,
